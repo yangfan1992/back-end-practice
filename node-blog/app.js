afed7cbd50bb144b5a130acbf46afc2023c5b34e
陈扬帆 <cyf3211@qq.com>
2017-11-27 17:03:32 +0800

    second commit

--- node-blog/app.js
+++ node-blog/app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var posts = require('./routes/posts');
+
 
 var app = express();
 
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
