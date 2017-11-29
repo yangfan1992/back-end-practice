080c70bf5bf3262305b65deb9ca5c3ca4e3862fc
陈扬帆 <cyf3211@qq.com>
2017-11-29 22:00:48 +0800

    优化配置

--- node-blog/config.js
+++ node-blog/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  cookieName: 'user_info'
+  cookieName: 'user_info',
+  mongodbUrl: 'mongodb://localhost:32770/node-blog'
 };
--- node-blog/models/init.js
+++ node-blog/models/init.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:32770/node-blog',{
+var config = require('../config');
+
+mongoose.connect(config.mongodbUrl, {
   userMongoClient: true
 });
