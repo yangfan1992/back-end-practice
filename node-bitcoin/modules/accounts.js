e15de2bbe6e0bc216379ce492a65f8b2db2ed652
陈扬帆 <cyf3211@qq.com>
2017-12-04 13:43:43 +0800

    accounts-private

--- node-bitcoin/modules/accounts.js
+++ node-bitcoin/modules/accounts.js
@@ -13,3 +13,129 @@ var crypto = require('crypto'),
 
 // privated fields
 var modules, library, self, privated = {}, shared = {};
+
+function Vote() {
+	this.create = function (data, trs) {
+		trs.recipientId = data.sender.address;
+		trs.recipientUsername = data.sender.username;
+		trs.asset.votes = data.votes;
+
+		return trs;
+	};
+
+	this.calculateFee = function (trs, sender) {
+		return 1 * constants.fixedPoint;
+	};
+
+	this.verify = function (trs, sender, cb) {
+		if (trs.recipientId != trs.senderId) {
+			return setImmediate(cb, "Recipient is identical to sender");
+		}
+
+		if (!trs.asset.votes || !trs.asset.votes.length) {
+			return setImmediate(cb, "Not enough spare votes available");
+		}
+
+		if (trs.asset.votes && trs.asset.votes.length > 33) {
+			return setImmediate(cb, "Voting limited exceeded. Maxmium is 33 per transaction");
+		}
+
+		modules.delegates.checkDelegates(trs.senderPublicKey, trs.asset.votes, function (err) {
+			setImmediate(cb, err, trs);
+		});
+	};
+
+	this.process = function (trs, sender, cb) {
+		setImmediate(cb, null, trs);
+  };
+  
+	this.getBytes = function (trs) {
+		try {
+			var buf = trs.asset.votes ? new Buffer(trs.asset.votes.join(''), 'utf8') : null;
+		} catch (e) {
+			throw Error(e.toString());
+		}
+
+		return buf;
+	};
+
+	this.apply = function (trs, block, sender, cb) {
+		this.scope.account.merge(sender.address, {
+			delegates: trs.asset.votes,
+			blockId: block.id,
+			round: modules.round.calc(block.height)
+		}, function (err) {
+			cb(err);
+		});
+	};
+
+	this.undo = function (trs, block, sender, cb) {
+		if (trs.asset.votes === null) return cb();
+
+		var votesInvert = Diff.reverse(trs.asset.votes);
+
+		this.scope.account.merge(sender.address, {
+			delegates: votesInvert,
+			blockId: block.id,
+			round: modules.round.calc(block.height)
+		}, function (err) {
+			cb(err);
+		});
+  };
+  
+  this.applyUnconfirmed = function (trs, sender, cb) {
+		modules.delegates.checkUnconfirmedDelegates(trs.senderPublicKey, trs.asset.votes, function (err) {
+			if (err) {
+				return setImmediate(cb, err);
+			}
+
+			this.scope.account.merge(sender.address, {
+				u_delegates: trs.asset.votes
+			}, function (err) {
+				cb(err);
+			});
+		}.bind(this));
+	};
+
+	this.undoUnconfirmed = function (trs, sender, cb) {
+		if (trs.asset.votes === null) return cb();
+
+		var votesInvert = Diff.reverse(trs.asset.votes);
+
+		this.scope.account.merge(sender.address, {u_delegates: votesInvert}, function (err) {
+			cb(err);
+		});
+  };
+  
+  this.objectNormalize = function (trs) {
+		var report = library.scheme.validate(trs.asset, {
+			type: "object",
+			properties: {
+				votes: {
+					type: "array",
+					minLength: 1,
+					maxLength: 105,
+					uniqueItems: true
+				}
+			},
+			required: ['votes']
+		});
+
+		if (!report) {
+			throw new Error("Incorrect votes in transactions: " + library.scheme.getLastError());
+		}
+
+		return trs;
+  };
+  
+  this.dbRead = function (raw) {
+		// console.log(raw.v_votes);
+
+		if (!raw.v_votes) {
+			return null;
+		} else {
+			var votes = raw.v_votes.split(',');
+
+			return {votes: votes};
+		}
+	};
